f6c25109bb82105ba79713dbef4598aef9b44076
shashank <shashank.bodkhe@gmail.com>
2025-02-23 19:03:42 +0530

    buildscpec change

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ const port = 3000;
 
 // Define a GET API
 app.get("/hello", (req, res) => {
-    res.json({ message: "Hello from Express.js!" });
+    res.json({ message: "Hello from eExpress.js!" });
 });
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+
